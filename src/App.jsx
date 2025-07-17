b556e42eeafd79fb4be09dc8ec5e43ab85fb53df
Snehil Hota <snehilhota13032006@gmail.com>
2025-07-17 18:18:10 +0530

    Added the player page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
   )
